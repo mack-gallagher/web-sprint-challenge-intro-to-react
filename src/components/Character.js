bbbcf1546802b69244a90b80a4eedfd327ba24c4
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-21 22:41:40 -0500

    pass all tests

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
-// Write your Character component here
+import styled from 'styled-components';
+
+function Character(props) {
+
+  const { name } = props;
+
+  const StyledCharacter = styled.div`
+    background-color: blue;
+    color: white;
+`
+
+  return (
+    <StyledCharacter id={name}>
+      <p>{name}</p>
+    </StyledCharacter>
+        );
+}
+
+export default Character
